b1a6dc3e25dc54fa2ff66a55cfac4e310c9f3485
cordy <mglgain@gmail.com>
2025-08-25 06:22:16 +0800

    Second Productions Release

--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,14 @@ import { useState, useEffect } from "react";
 import { Header, Banner, ChatBot } from '@/app/components/GlobalImports';
 import config from "@/app/config/conf/setting";
 
-const PROJECT_ID = process.env.NEXT_PUBLIC_LINK_PROJECT_ID || config.PUBLIC_ACCESS.RPC_Endpoint;
+const RPC_Endpoint = process.env.NEXT_PUBLIC_LINK_PROJECT_ID || config.PUBLIC_ACCESS.RPC_Endpoint;
 
 export default function Home() {
   const [showChatBot, setShowChatBot] = useState(false);
   const [warning, setWarning] = useState(true);
 
-  
-
-  
-
   useEffect(() => {
-    if (!PROJECT_ID) {
+    if (!RPC_Endpoint) {
       setWarning(false);
       
     }
@@ -24,7 +20,7 @@ export default function Home() {
     <main>
       <div className="Home">
 
-        <div className={`${warning ? "hidden" : "warning"}`}><p>⚠️ Missing <u>PROJECT_ID</u> at src/app/config/conf/setting.json</p></div>
+        <div className={`${warning ? "hidden" : "warning"}`}><p>⚠️ Missing <u>RPC_Endpoint</u> at src/app/config/conf/setting.json</p></div>
 
         <Header />
 
@@ -33,7 +29,8 @@ export default function Home() {
         <ChatBot visible={showChatBot} onClose={() => setShowChatBot(false)} />
 
         <footer>
-
+            <a href="https://lcc.edu.ph/">© La Consolacion College Bacolod</a>
+            <p>Developed by <a href="https://cordy-portfolio.vercel.app/">Cordy</a></p>
         </footer>
       </div>
     </main>
